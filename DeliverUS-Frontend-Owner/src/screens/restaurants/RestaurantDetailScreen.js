ba65dc2bfba5fd4550f8edc59b2f4a4faaa40f15
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-03-17 13:54:15 +0100

    Render header implemented

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -1,13 +1,27 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, FlatList, Pressable } from 'react-native'
+import { StyleSheet, View, FlatList, Pressable, ImageBackground, Image } from 'react-native'
 import TextRegular from '../../components/TextRegular'
 import { API_BASE_URL } from '@env'
 import { getDetail } from '../../api/RestaurantEndpoints'
+import TextSemiBold from '../../components/TextSemibold'
+import { brandSecondary } from '../../styles/GlobalStyles'
 
-export default function RestaurantDetailScreen({ route }) {
+export default function RestaurantDetailScreen ({ route }) {
   const { id } = route.params
-  const [restaurants, setRestaurants] = useState({})
+  const [restaurant, setRestaurants] = useState({})
+
+  const renderHeader = () => {
+    return (
+      <ImageBackground source={(restaurant?.heroImage) ? { uri: process.env.API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined } style={styles.imageBackground}>
+        <View style={styles.restaurantHeaderContainer}>
+            <TextSemiBold textStyle={styles.textTitle}>{restaurant.name}</TextSemiBold>
+            <Image style={styles.image} source={restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + restaurant.logo, cache: 'force-cache' } : undefined} />
+            <TextRegular textStyle={styles.text}>{restaurant.description}</TextRegular>
+        </View>
+      </ImageBackground>
+    )
+  }
 
   useEffect(() => {
     console.log('Loading restaurant details, please wait 1 second')
@@ -31,13 +45,14 @@ export default function RestaurantDetailScreen({ route }) {
   return (
     <View style={styles.container}>
       <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>Restaurant details. Id: {id}</TextRegular>
-      <TextRegular style={styles.textTitle}>{restaurants.name}</TextRegular>
-            <TextRegular style={styles.text}>{restaurants.description}</TextRegular>
-            <TextRegular style={styles.text}>shippingCosts: {restaurants.shippingCosts}</TextRegular>
+      <TextRegular style={styles.textTitle}>{restaurant.name}</TextRegular>
+            <TextRegular style={styles.text}>{restaurant.description}</TextRegular>
+            <TextRegular style={styles.text}>shippingCosts: {restaurant.shippingCosts}</TextRegular>
             <FlatList
               style={styles.container}
-              data={restaurants.products}
+              data={restaurant.products}
               renderItem={renderProduct}
+              ListHeaderComponent={renderHeader}
               keyExtractor={item => item.id.toString()}
             />
     </View>
@@ -47,5 +62,34 @@ export default function RestaurantDetailScreen({ route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: brandSecondary
+  },
+  restaurantHeaderContainer: {
+    height: 250,
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 100,
+    width: 100,
+    margin: 10
+  },
+  text: {
+    color: 'black'
+  },
+  textTitle: {
+    fontSize: 20,
+    color: 'black'
   }
 })
