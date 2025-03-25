cc427c9be7f0417b9e748bf32ac3acf49a9fa126
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-03-25 18:15:40 +0100

    Detail Screen with header

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -2,18 +2,17 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, FlatList, Pressable, ImageBackground, Image } from 'react-native'
 import TextRegular from '../../components/TextRegular'
-import { API_BASE_URL } from '@env'
 import { getDetail } from '../../api/RestaurantEndpoints'
 import TextSemiBold from '../../components/TextSemibold'
 import { brandSecondary } from '../../styles/GlobalStyles'
 
 export default function RestaurantDetailScreen ({ route }) {
   const { id } = route.params
-  const [restaurant, setRestaurants] = useState({})
+  const [restaurant, setRestaurants] = useState([])
 
   const renderHeader = () => {
     return (
-      <ImageBackground source={(restaurant?.heroImage) ? { uri: process.env.API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined } style={styles.imageBackground}>
+      <ImageBackground source={(restaurant.heroImage) ? { uri: process.env.API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined } style={styles.imageBackground}>
         <View style={styles.restaurantHeaderContainer}>
             <TextSemiBold textStyle={styles.textTitle}>{restaurant.name}</TextSemiBold>
             <Image style={styles.image} source={restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + restaurant.logo, cache: 'force-cache' } : undefined} />
@@ -44,17 +43,13 @@ export default function RestaurantDetailScreen ({ route }) {
   }
   return (
     <View style={styles.container}>
-      <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>Restaurant details. Id: {id}</TextRegular>
-      <TextRegular style={styles.textTitle}>{restaurant.name}</TextRegular>
-            <TextRegular style={styles.text}>{restaurant.description}</TextRegular>
-            <TextRegular style={styles.text}>shippingCosts: {restaurant.shippingCosts}</TextRegular>
-            <FlatList
-              style={styles.container}
-              data={restaurant.products}
-              renderItem={renderProduct}
-              ListHeaderComponent={renderHeader}
-              keyExtractor={item => item.id.toString()}
-            />
+      <FlatList
+        style={styles.container}
+        data={restaurant.products}
+        renderItem={renderProduct}
+        ListHeaderComponent={renderHeader}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   )
 }
@@ -86,10 +81,10 @@ const styles = StyleSheet.create({
     margin: 10
   },
   text: {
-    color: 'black'
+    color: 'white'
   },
   textTitle: {
     fontSize: 20,
-    color: 'black'
+    color: 'white'
   }
 })
